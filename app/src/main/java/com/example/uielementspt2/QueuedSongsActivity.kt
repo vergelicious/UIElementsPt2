799875df5424ad431fb0523c484e0c2aef0b0a64
vergelicious <vergel945@gmail.com>
2020-11-17 18:58:02 +0800

    Rename the activity

--- app/src/main/java/com/example/uielementspt2/QueuedSongsActivity.kt
+++ app/src/main/java/com/example/uielementspt2/QueuedSongsActivity.kt
@@ -1,21 +1,14 @@
 package com.example.uielementspt2
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.ContextMenu
-import android.view.MenuItem
-import android.view.View
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
-import android.widget.Toast
 
 class QueuedSongsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_queuedsongs)
+        setContentView(R.layout.activity_queued_songs)
 
         val songsDisplayList = intent.getStringArrayListExtra("songs")
         val songsDisplayArray = songsDisplayList!!.toTypedArray()
